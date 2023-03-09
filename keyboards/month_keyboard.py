584bdb45d292a8864f788aa7f5b3a7ac1156b27e
bublik911 <110043747+bublik911@users.noreply.github.com>
2023-03-09 23:29:05 +0300

    Клавиатура с месяцами

---
+++ keyboards/month_keyboard.py
@@ -0,0 +1,11 @@
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import ReplyKeyboardMarkup
+
+
+def month_keyboard() -> ReplyKeyboardMarkup:
+    months =["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+    keyboard = ReplyKeyboardBuilder()
+    for month in months:
+        keyboard.button(text=month)
+    keyboard.adjust(3)
+    return keyboard.as_markup(resize_keyboard=True)
