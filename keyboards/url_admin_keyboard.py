441119f9166ca856b36385ebfff8b616233e1716
bublik911 <110043747+bublik911@users.noreply.github.com>
2023-03-09 23:32:07 +0300

    Добавлен тип возвращаемого значения

--- keyboards/url_admin_keyboard.py
+++ keyboards/url_admin_keyboard.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def url_admin_keyboard():
+def url_admin_keyboard() -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardBuilder()
     keyboard.button(text="Администратор", url="https://t.me/bublik_c_chaem")
     return keyboard.as_markup()
